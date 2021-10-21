cd2b46c6e5125d42662ce060692454731d5434cc
github username <yoshimejia00@gmail.comgit config --global user.name Yoshi>
2021-10-21 15:10:52 -0400

    commit tree fec0e85dfdeb622ff0d0a080b071111e0242e1fa

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Your code here
+function saturdayFun(activity="roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+
+const mondayWork = function(activity="go to the office"){
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(str="*"){
+    return function(anonArg="special"){
+        return `You are ${str}${anonArg}${str}!`
+    }
+}
+
+const Calculator = {
+    add: function(num1, num2){
+        return num1 + num2
+    },
+    multiply: function(num1, num2){
+        return num1 * num2
+    },
+    divide: function(num1, num2){
+        return num1 / num2
+    },
+    subtract: function(num1, num2){
+        return num1 - num2
+    }
+}
+
+let actionApplyer = function(start, ray) {
+    let a = start
+  
+    for (let i = 0; i < ray.length; i++ ){
+      a = ray[i](a)
+    }  
+    return a
+  }
